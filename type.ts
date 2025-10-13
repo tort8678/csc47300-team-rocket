6ea2fa41eb710a7eb2c3a6bc24dddca8e4ea2226
tort8678 <102439231+tort8678@users.noreply.github.com>
2025-10-13 17:18:36 -0400

    Added JSON types

---
+++ type.ts
@@ -0,0 +1,24 @@
+interface User {
+    id: string;
+    name: string;
+    password: string;
+    profilePictureUrl: string;
+    email: string;
+    bio: string;
+    createdAt: Date;
+}
+
+interface Post {
+    id: string;
+    threadId: string;
+    authorId: string;
+    content: string;
+    createdAt: Date;
+}
+
+interface Thread {
+    id: string;
+    title: string;
+    createdAt: Date;
+    category: string;
+}
